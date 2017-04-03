abc05fb588225565b13d68eb9917ec1db803da40
Mark Goldsmith (AU) <mark.goldsmith@didata.com.au>
2017-04-04 08:40:53 +1000

    Implement session flag negotiation for NTLMv2 Updates to Negotiate/Challenge/Authenticate message exchange for NTLMv2 connection oriented mode

--- ntlm/ntlmv2.go
+++ ntlm/ntlmv2.go
@@ -35,6 +35,107 @@ func (n *V2Session) SetMode(mode Mode) {
 	n.mode = mode
 }
 
+// SetConfigFlags sets the client/server configuration flags (depending upon the session type) used to negotiate the NTLM session
+func (n *V2Session) SetConfigFlags(flags uint32) (err error) {
+
+	if err = n.CheckNegotiateFlags(flags); err != nil {
+		return err
+	}
+
+	n.configFlags = flags
+	return nil
+}
+
+// InitialNegotiateFlags returns the desired set of negotiated flags. They are used to start the session negotiation process for both connection oriented and
+// connectionless modes
+func (n *V2Session) InitialNegotiateFlags() (flags uint32) {
+
+	// 3.1.5.1.1: The client sets the following configuration flags in the NegotiateFlags field of the NEGOTIATE_MESSAGE
+	flags = uint32(0)
+	flags = NTLMSSP_REQUEST_TARGET.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_NTLM.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_ALWAYS_SIGN.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_UNICODE.Set(flags)
+
+	// In addition, the client sets the flags specified by the application in the NegotiateFlags field in addition to the initialized flags
+	flags |= n.configFlags
+
+	// If LM authentication is not being used, then the client sets the following configuration flag in the NegotiateFlags field of the NEGOTIATE_MESSAGE
+	if !NTLMSSP_NEGOTIATE_LM_KEY.IsSet(flags) {
+		NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY.Set(flags)
+	}
+
+	return flags
+}
+
+// AcceptNegotiateFlags reviews the requested flags and produces a set of agreed flags that can be supported by the client & server
+func (n *V2Session) AcceptNegotiateFlags(reqFlags uint32) (flags uint32, err error) {
+
+	flags = uint32(0)
+
+	// Set only the supported flags that were requested in the CHALLENGE_MESSAGE.NegotiateFlags
+	flags |= reqFlags & n.GetSupportedNegotiateFlags()
+
+	flags = NTLMSSP_NEGOTIATE_NTLM.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_ALWAYS_SIGN.Set(flags)
+
+	if NTLMSSP_NEGOTIATE_UNICODE.IsSet(reqFlags) {
+		flags = NTLMSSP_NEGOTIATE_UNICODE.Set(flags)
+	} else if NTLM_NEGOTIATE_OEM.IsSet(reqFlags) {
+		// OEM mode not supported. Generate error
+		// NTLMSSP_NEGOTIATE_OEM.Set(flags)
+		return flags, errors.New("OEM encoding not supported")
+	}
+
+	if NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY.IsSet(reqFlags) {
+		flags = NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY.Set(flags)
+	} else if NTLMSSP_NEGOTIATE_LM_KEY.IsSet(reqFlags) {
+		flags = NTLMSSP_NEGOTIATE_LM_KEY.Set(flags)
+	}
+
+	return flags, nil
+}
+
+// CheckNegotiateFlags checks that the flags provided are supported and meet the minimum security requirements
+func (n *V2Session) CheckNegotiateFlags(flags uint32) (err error) {
+
+	// Check that all flags are supported
+	if unsupportedFlags := flags &^ n.GetSupportedNegotiateFlags(); unsupportedFlags != 0 {
+		err := fmt.Errorf("Config flags contain unsupported flags (%08X)", unsupportedFlags)
+		return err
+	}
+
+	return nil
+}
+
+// GetSupportedNegotiateFlags returns the full set of NTLM flags supported by this library
+func (n *V2Session) GetSupportedNegotiateFlags() (flags uint32) {
+	flags = uint32(0)
+	flags = NTLMSSP_NEGOTIATE_56.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_KEY_EXCH.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_128.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_VERSION.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_TARGET_INFO.Set(flags)
+	flags = NTLMSSP_REQUEST_NON_NT_SESSION_KEY.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_IDENTIFY.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY.Set(flags)
+	flags = NTLMSSP_TARGET_TYPE_SERVER.Set(flags)
+	flags = NTLMSSP_TARGET_TYPE_DOMAIN.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_ALWAYS_SIGN.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED.Set(flags)
+	//NTLMSSP_ANONYMOUS not supported
+	flags = NTLMSSP_NEGOTIATE_NTLM.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_LM_KEY.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_DATAGRAM.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_SEAL.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_SIGN.Set(flags)
+	flags = NTLMSSP_REQUEST_TARGET.Set(flags)
+	//NTLM_NEGOTIATE_OEM not supported
+	flags = NTLMSSP_NEGOTIATE_UNICODE.Set(flags)
+	return flags
+}
+
 func (n *V2Session) Version() int {
 	return 2
 }
@@ -45,10 +146,6 @@ func (n *V2Session) fetchResponseKeys() (err error) {
 	n.responseKeyLM = lmowfv2(n.user, n.password, n.userDomain)
 	n.responseKeyNT = ntowfv2(n.user, n.password, n.userDomain)
 	return
-}
-
-func (n *V2ServerSession) GetSessionData() *SessionData {
-	return &n.SessionData
 }
 
 // Define ComputeResponse(NegFlg, ResponseKeyNT, ResponseKeyLM, CHALLENGE_MESSAGE.ServerChallenge, ClientChallenge, Time, ServerName)
@@ -117,6 +214,10 @@ type V2ServerSession struct {
 	V2Session
 }
 
+func (n *V2ServerSession) GetSessionData() *SessionData {
+	return &n.SessionData
+}
+
 func (n *V2ServerSession) SetServerChallenge(challenge []byte) {
 	n.serverChallenge = challenge
 }
@@ -132,6 +233,19 @@ func (n *V2ServerSession) SetTargetInfo(domainJoined bool, nbMachineName, nbDoma
 
 func (n *V2ServerSession) ProcessNegotiateMessage(nm *NegotiateMessage) (err error) {
 	n.negotiateMessage = nm
+
+	// Process the flags requested by client and accept what we can support/are our preferences
+	flags, err := n.AcceptNegotiateFlags(nm.NegotiateFlags)
+	if err != nil {
+		return err
+	}
+
+	// Confirm that the client authentication policy has been met
+	if err = n.CheckNegotiateFlags(flags); err != nil {
+		return err
+	}
+
+	n.NegotiateFlags = flags
 	return
 }
 
@@ -139,21 +253,29 @@ func (n *V2ServerSession) GenerateChallengeMessage() (cm *ChallengeMessage, err 
 	cm = new(ChallengeMessage)
 	cm.Signature = []byte("NTLMSSP\x00")
 	cm.MessageType = uint32(2)
-	cm.TargetName, _ = CreateBytePayload(make([]byte, 0))
 
-	flags := uint32(0)
-	flags = NTLMSSP_NEGOTIATE_KEY_EXCH.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_VERSION.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_TARGET_INFO.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_IDENTIFY.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_ALWAYS_SIGN.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_NTLM.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_DATAGRAM.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_SIGN.Set(flags)
+	var flags uint32
+	if NTLMSSP_NEGOTIATE_DATAGRAM.IsSet(n.configFlags) {
+		// Connectionless mode - set initial config flags
+		flags = n.InitialNegotiateFlags()
+
+	} else {
+		// Connection oriented mode - use flags from client
+		flags = n.NegotiateFlags
+	}
+
 	flags = NTLMSSP_REQUEST_TARGET.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_UNICODE.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_128.Set(flags)
+	flags = NTLMSSP_NEGOTIATE_TARGET_INFO.Set(flags)
+
+	if NTLMSSP_REQUEST_TARGET.IsSet(flags) {
+		if n.domainJoined {
+			cm.TargetName, _ = CreateStringPayload(n.nbDomainName)
+			flags = NTLMSSP_TARGET_TYPE_DOMAIN.Set(flags)
+		} else {
+			cm.TargetName, _ = CreateStringPayload(n.nbMachineName)
+			flags = NTLMSSP_TARGET_TYPE_SERVER.Set(flags)
+		}
+	}
 
 	cm.NegotiateFlags = flags
 
@@ -161,41 +283,59 @@ func (n *V2ServerSession) GenerateChallengeMessage() (cm *ChallengeMessage, err 
 	cm.ServerChallenge = n.serverChallenge
 	cm.Reserved = make([]byte, 8)
 
-	// Create the AvPairs we need
-	pairs := new(AvPairs)
-	if len(n.nbMachineName) > 0 {
-		pairs.AddAvPair(MsvAvNbComputerName, utf16FromString(n.nbMachineName))
+	if NTLMSSP_NEGOTIATE_TARGET_INFO.IsSet(flags) {
+		// Create the AvPairs we need
+		pairs := new(AvPairs)
+		if len(n.nbMachineName) > 0 {
+			pairs.AddAvPair(MsvAvNbComputerName, utf16FromString(n.nbMachineName))
+		}
+		if len(n.nbDomainName) > 0 {
+			pairs.AddAvPair(MsvAvNbDomainName, utf16FromString(n.nbDomainName))
+		}
+		if len(n.dnsMachineName) > 0 {
+			pairs.AddAvPair(MsvAvDnsComputerName, utf16FromString(n.dnsMachineName))
+		}
+		if len(n.dnsDomainName) > 0 {
+			pairs.AddAvPair(MsvAvDnsDomainName, utf16FromString(n.dnsDomainName))
+		}
+		if len(n.dnsForestName) > 0 {
+			pairs.AddAvPair(MsvAvDnsTreeName, utf16FromString(n.dnsForestName))
+		}
+		pairs.AddAvPair(MsvAvEOL, make([]byte, 0))
+		cm.TargetInfo = pairs
+		cm.TargetInfoPayloadStruct, _ = CreateBytePayload(pairs.Bytes())
 	}
-	if len(n.nbDomainName) > 0 {
-		pairs.AddAvPair(MsvAvNbDomainName, utf16FromString(n.nbDomainName))
-	}
-	if len(n.dnsMachineName) > 0 {
-		pairs.AddAvPair(MsvAvDnsComputerName, utf16FromString(n.dnsMachineName))
-	}
-	if len(n.dnsDomainName) > 0 {
-		pairs.AddAvPair(MsvAvDnsDomainName, utf16FromString(n.dnsDomainName))
-	}
-	if len(n.dnsForestName) > 0 {
-		pairs.AddAvPair(MsvAvDnsTreeName, utf16FromString(n.dnsForestName))
-	}
-	pairs.AddAvPair(MsvAvEOL, make([]byte, 0))
-	cm.TargetInfo = pairs
-	cm.TargetInfoPayloadStruct, _ = CreateBytePayload(pairs.Bytes())
 
-	cm.Version = &VersionStruct{ProductMajorVersion: uint8(5), ProductMinorVersion: uint8(1), ProductBuild: uint16(2600), NTLMRevisionCurrent: uint8(15)}
+	if NTLMSSP_NEGOTIATE_VERSION.IsSet(cm.NegotiateFlags) {
+		cm.Version = &VersionStruct{ProductMajorVersion: uint8(5), ProductMinorVersion: uint8(1), ProductBuild: uint16(2600), NTLMRevisionCurrent: uint8(15)}
+	}
+
+	// Connectionless mode - save negotiate flags to session
+	if NTLMSSP_NEGOTIATE_DATAGRAM.IsSet(n.configFlags) {
+		n.NegotiateFlags = cm.NegotiateFlags
+	}
+
+	n.challengeMessage = cm
 	return cm, nil
 }
 
 func (n *V2ServerSession) ProcessAuthenticateMessage(am *AuthenticateMessage) (err error) {
 	n.authenticateMessage = am
+
+	// Confirm that the client authentication policy has been met
+	if err = n.CheckNegotiateFlags(am.NegotiateFlags); err != nil {
+		return err
+	}
+
 	n.NegotiateFlags = am.NegotiateFlags
 	n.clientChallenge = am.ClientChallenge()
 	n.encryptedRandomSessionKey = am.EncryptedRandomSessionKey.Payload
+
 	// Ignore the values used in SetUserInfo and use these instead from the authenticate message
 	// They should always be correct (I hope)
 	n.user = am.UserName.String()
 	n.userDomain = am.DomainName.String()
-	log.Printf("(ProcessAuthenticateMessage)NTLM v2 User %s Domain %s", n.user, n.userDomain)
+	log.Printf("(ProcessAuthenticateMessage)NTLM v2 User '%s' Domain '%s'", n.user, n.userDomain)
 
 	err = n.fetchResponseKeys()
 	if err != nil {
@@ -233,7 +373,6 @@ func (n *V2ServerSession) ProcessAuthenticateMessage(am *AuthenticateMessage) (e
 		//UGH not entirely sure how this could possibly happen, going to put this in for now
 		//TODO investigate if this ever is really happening
 		am.Version = &VersionStruct{ProductMajorVersion: uint8(5), ProductMinorVersion: uint8(1), ProductBuild: uint16(2600), NTLMRevisionCurrent: uint8(15)}
-
 		log.Printf("Nil version in ntlmv2")
 	}
 
@@ -325,66 +464,45 @@ func (n *V2ClientSession) SetMachineName(nbMachineName string) {
 }
 
 func (n *V2ClientSession) GenerateNegotiateMessage() (nm *NegotiateMessage, err error) {
-	return nil, nil
+	nm = new(NegotiateMessage)
+	nm.Signature = []byte("NTLMSSP\x00")
+	nm.MessageType = uint32(1)
+
+	nm.NegotiateFlags = n.InitialNegotiateFlags()
+
+	if NTLMSSP_NEGOTIATE_VERSION.IsSet(nm.NegotiateFlags) {
+		nm.WorkstationFields, _ = CreateStringPayload("")
+		nm.DomainNameFields, _ = CreateStringPayload("")
+		nm.Version = &VersionStruct{ProductMajorVersion: uint8(5), ProductMinorVersion: uint8(1), ProductBuild: uint16(2600), NTLMRevisionCurrent: uint8(15)}
+	}
+
+	// Save to represent the current set of negotiated flags
+	n.NegotiateFlags = nm.NegotiateFlags
+
+	return nm, nil
 }
 
 func (n *V2ClientSession) ProcessChallengeMessage(cm *ChallengeMessage) (err error) {
 	n.challengeMessage = cm
 	n.serverChallenge = cm.ServerChallenge
-	n.clientChallenge = randomBytes(8)
 
-	// Set up the default flags for processing the response. These are the flags that we will return
-	// in the authenticate message
-	flags := uint32(0)
-	flags = NTLMSSP_NEGOTIATE_KEY_EXCH.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_VERSION.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_TARGET_INFO.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_IDENTIFY.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_ALWAYS_SIGN.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_NTLM.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_DATAGRAM.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_SIGN.Set(flags)
-	flags = NTLMSSP_REQUEST_TARGET.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_UNICODE.Set(flags)
-	flags = NTLMSSP_NEGOTIATE_128.Set(flags)
+	// Produce the agreed set of flags for this session
+	flags, err := n.AcceptNegotiateFlags(cm.NegotiateFlags)
+	if err != nil {
+		return err
+	}
+
+	// Confirm that the client authentication policy has been met
+	if err = n.CheckNegotiateFlags(flags); err != nil {
+		return err
+	}
 
 	n.NegotiateFlags = flags
 
-	err = n.fetchResponseKeys()
-	if err != nil {
-		return err
-	}
+	// Initialisation of these independent variables done here to provide an opportunity to override in test cases. See TestNTLMv2ClientAuthentication for example
+	n.clientChallenge = randomBytes(8)
+	n.exportedSessionKey = randomBytes(16)
 
-	timestamp := timeToWindowsFileTime(time.Now())
-	err = n.computeExpectedResponses(timestamp, cm.TargetInfoPayloadStruct.Payload)
-	if err != nil {
-		return err
-	}
-
-	err = n.computeKeyExchangeKey()
-	if err != nil {
-		return err
-	}
-
-	err = n.computeEncryptedSessionKey()
-	if err != nil {
-		return err
-	}
-
-	err = n.calculateKeys(cm.Version.NTLMRevisionCurrent)
-	if err != nil {
-		return err
-	}
-
-	n.clientHandle, err = rc4Init(n.ClientSealingKey)
-	if err != nil {
-		return err
-	}
-	n.serverHandle, err = rc4Init(n.ServerSealingKey)
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
@@ -392,6 +510,43 @@ func (n *V2ClientSession) GenerateAuthenticateMessage() (am *AuthenticateMessage
 	am = new(AuthenticateMessage)
 	am.Signature = []byte("NTLMSSP\x00")
 	am.MessageType = uint32(3)
+	cm := n.challengeMessage
+
+	err = n.fetchResponseKeys()
+	if err != nil {
+		return nil, err
+	}
+
+	timestamp := timeToWindowsFileTime(time.Now())
+	err = n.computeExpectedResponses(timestamp, cm.TargetInfoPayloadStruct.Payload)
+	if err != nil {
+		return nil, err
+	}
+
+	err = n.computeKeyExchangeKey()
+	if err != nil {
+		return nil, err
+	}
+
+	err = n.computeEncryptedSessionKey()
+	if err != nil {
+		return nil, err
+	}
+
+	err = n.calculateKeys(cm.Version.NTLMRevisionCurrent)
+	if err != nil {
+		return nil, err
+	}
+
+	n.clientHandle, err = rc4Init(n.ClientSealingKey)
+	if err != nil {
+		return nil, err
+	}
+	n.serverHandle, err = rc4Init(n.ServerSealingKey)
+	if err != nil {
+		return nil, err
+	}
+
 	am.LmChallengeResponse, _ = CreateBytePayload(n.lmChallengeResponse)
 	am.NtChallengeResponseFields, _ = CreateBytePayload(n.ntChallengeResponse)
 	am.DomainName, _ = CreateStringPayload(n.userDomain)
@@ -406,12 +561,12 @@ func (n *V2ClientSession) GenerateAuthenticateMessage() (am *AuthenticateMessage
 
 func (n *V2ClientSession) computeEncryptedSessionKey() (err error) {
 	if NTLMSSP_NEGOTIATE_KEY_EXCH.IsSet(n.NegotiateFlags) {
-		n.exportedSessionKey = randomBytes(16)
 		n.encryptedRandomSessionKey, err = rc4K(n.keyExchangeKey, n.exportedSessionKey)
 		if err != nil {
 			return err
 		}
 	} else {
+		n.exportedSessionKey = n.keyExchangeKey
 		n.encryptedRandomSessionKey = n.keyExchangeKey
 	}
 	return nil
